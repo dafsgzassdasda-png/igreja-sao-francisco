40406e35ba778c6ed44ddf4d9cf9a60afe92b2d1
dafsgzassdasda-png <dafsgzassdasda@gmail.com>
2026-03-23 14:11:47 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,187 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-centro-da-igrejajpg",
+      "name": "centro da igreja.jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.566024058815877,
+          "pitch": 0.5217937313052055,
+          "rotation": 0,
+          "target": "3-pergoladojpg"
+        },
+        {
+          "yaw": -1.423201408818592,
+          "pitch": 0.5426234887508699,
+          "rotation": 0,
+          "target": "2-parte-de-tras-da-igrejajpg"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-parte-de-fora-jpg",
+      "name": "PARTE DE FORA .jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5701286324165151,
+          "pitch": 0.5752721174008961,
+          "rotation": 0,
+          "target": "3-pergoladojpg"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-parte-de-tras-da-igrejajpg",
+      "name": "parte de tras da igreja.jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4165849066376826,
+          "pitch": 0.374012046722779,
+          "rotation": 0,
+          "target": "0-centro-da-igrejajpg"
+        },
+        {
+          "yaw": -0.6949974435493953,
+          "pitch": 0.6780025523347142,
+          "rotation": 0,
+          "target": "3-pergoladojpg"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-pergoladojpg",
+      "name": "PERGOLADO.jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.108588058746772,
+          "pitch": 0.5979224308451432,
+          "rotation": 0,
+          "target": "1-parte-de-fora-jpg"
+        },
+        {
+          "yaw": -1.2482709443900877,
+          "pitch": 0.44367640678925646,
+          "rotation": 0,
+          "target": "2-parte-de-tras-da-igrejajpg"
+        },
+        {
+          "yaw": 1.1965411016811576,
+          "pitch": 0.48266719998574636,
+          "rotation": 5.497787143782138,
+          "target": "0-centro-da-igrejajpg"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "igreja sao francisco",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
